d0205cca144cac3a947f65b9c68af6508a2c0dba
Alexey Pargachev <pargachev20@gmail.com>
2021-07-20 17:42:37 +0700

    add sidebar

---
+++ src/components/SidebarData.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+import * as RiIcons from 'react-icons/ri';
+
+export const SidebarData = [
+  {
+    title: 'Overview',
+    path: '/overview',
+    icon: <AiIcons.AiFillHome />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpen: <RiIcons.RiArrowUpSFill />,
+    subNav: [
+      { title: 'Users', path: '/overview/users', icon: <IoIcons.IoIosPaper /> },
+      {
+        title: 'Revenue',
+        path: '/overview/revenue',
+        icon: <IoIcons.IoIosPaper />,
+      },
+    ],
+  },
+  {
+    title: 'Reports',
+    path: '/reports',
+    icon: <AiIcons.AiFillHome />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpen: <RiIcons.RiArrowUpSFill />,
+    subNav: [
+      {
+        title: 'Reports',
+        path: '/reports/reports1',
+        icon: <IoIcons.IoIosPaper />,
+      },
+      {
+        title: 'Reports 2',
+        path: '/reports/reports2',
+        icon: <IoIcons.IoIosPaper />,
+      },
+      {
+        title: 'Reports 3',
+        path: '/reports/reports3',
+        icon: <IoIcons.IoIosPaper />,
+      },
+    ],
+  },
+  {
+    title: 'Products',
+    path: '/products',
+    icon: <FaIcons.FaCartPlus />,
+  },
+  {
+    title: 'Team',
+    path: '/team',
+    icon: <IoIcons.IoMdPeople />,
+  },
+];
